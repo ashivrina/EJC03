ba7643fb4f93de95a7e3a601186711c57b79e309
annashivrina <annashivrina@gmail.com>
2017-10-09 00:13:36 +0300

    Correction for homework

--- src/main/java/com/epam/jcs/Homework1.java
+++ src/main/java/com/epam/jcs/Homework1.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class hw1 {
+public class Homework1 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
         String enteredStr = reader.readLine();
