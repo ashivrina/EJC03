5fb06214feb2cee30cbf7b1cf619768e5792cdaa
annashivrina <annashivrina@gmail.com>
2017-10-08 23:56:07 +0300

    Correction for homework

--- src/main/java/com/epam/jcs/hw1.java
+++ src/main/java/com/epam/jcs/hw1.java
@@ -10,16 +10,18 @@ public class hw1 {
         String enteredStr = reader.readLine();
         int numOfBottles = Integer.parseInt(enteredStr);
 
-        for (int i = numOfBottles; i >= 0; i--){
-            if (i == 1){
-                System.out.println(i + " bottle standing on the wall.");
+        if (numOfBottles >= 0) {
+            for (int i = numOfBottles; i >= 0; i--) {
+                if (i == 1) {
+                    System.out.println(i + " bottle standing on the wall.");
+                } else if (i == 0) {
+                    System.out.println("All bottles've fallen. Go buy some more.");
+                } else {
+                    System.out.println(i + " bottles standing on the wall.");
+                }
             }
-            else if (i == 0){
-                System.out.println("All bottles've fallen. Go buy some more.");
-            }
-            else {
-            System.out.println(i + " bottles standing on the wall.");
-            }
+        } else {
+            System.out.println("Incorrect number entered. Please enter a positive number or 0.");
         }
     }
 }
