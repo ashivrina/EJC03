e6c5a17516cab148f6688f7ce44cbc6488557db3
annashivrina <annashivrina@gmail.com>
2017-10-14 02:06:51 +0300

    Correction for traffic light class.

--- src/main/java/com/epam/jcs/hm2/TrafficLight.java
+++ src/main/java/com/epam/jcs/hm2/TrafficLight.java
@@ -11,16 +11,28 @@ import java.io.InputStreamReader;
  */
 
 public class TrafficLight {
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
+
+        System.out.println(determineColour(readNumber()));
+    }
+
+    /**
+     * Method that reads a number from console and returns a number of minutes.
+     *
+     * @return parsed number of minutes entered by a user
+     */
+    private static int readNumber() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Please enter a number of minutes.");
-        String enteredString = reader.readLine();
-        int enteredNumber = Integer.parseInt(enteredString);
-        if (enteredNumber >= 0) {
-            System.out.println(determineColour(enteredNumber));
-        } else {
-            System.out.println("Please enter a positive integer");
+        String enteredString = null;
+        try {
+            enteredString = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        int enteredNumber = Integer.parseInt(enteredString);
+        return enteredNumber;
     }
 
     /**
@@ -29,14 +41,20 @@ public class TrafficLight {
      * @param minutes - entered number of minutes
      * @return color of the traffic light
      */
-    private static String determineColour(int minutes) {
+    public static String determineColour(int minutes) {
         int cleanNumberOfMinutes = minutes % 60;
+        int redLightMinutes = 2;
+        int yellowLightMinutes = 3;
+        int greenLightMinutes = 5;
+        int trafficLightCycleMinutes = redLightMinutes + yellowLightMinutes + greenLightMinutes;
         String color;
-        if ((cleanNumberOfMinutes % 10 >= 0) && (cleanNumberOfMinutes % 10 < 2)) {
+        if (cleanNumberOfMinutes < 0) {
+            color = "Undefined. Enter a positive integer.";
+        } else if (cleanNumberOfMinutes % trafficLightCycleMinutes < redLightMinutes) {
             color = "Red";
-        } else if ((cleanNumberOfMinutes % 10 >= 2) && (cleanNumberOfMinutes % 10 < 5)) {
+        } else if ((cleanNumberOfMinutes % trafficLightCycleMinutes >= redLightMinutes) && (cleanNumberOfMinutes % trafficLightCycleMinutes < redLightMinutes + yellowLightMinutes)) {
             color = "Yellow";
-        } else if (cleanNumberOfMinutes % 10 >= 5) {
+        } else if (cleanNumberOfMinutes % trafficLightCycleMinutes >= redLightMinutes + yellowLightMinutes) {
             color = "Green";
         } else {
             color = "Undefined. Don't know how this could have happened!";
